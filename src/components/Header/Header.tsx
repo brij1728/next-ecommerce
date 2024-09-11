adac663ec2e3e7ba31f54cca2dd78d09d5643f8f
Brijesh Kumar <bk10895@gmail.com>
2024-09-11 18:14:42 +0530

    Update Header component to include a heading tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const Header = () => {
   return (
-	<div>Header</div>
-  )
-}
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
